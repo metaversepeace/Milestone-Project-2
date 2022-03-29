5fc698d2be13374282574542c62d771a3471b529
metaversepeace <89940958+metaversepeace@users.noreply.github.com>
2022-03-28 20:18:14 -0700

    changed name to title for consistency

--- client/src/components/Lesson-view.js
+++ client/src/components/Lesson-view.js
@@ -6,14 +6,14 @@ export default function Home () {
    return (
        <Def>
            <main>
-               <h1>BEWARE, I AM SINNISTAR! THIS IS THE {lesson.name} Lesson!</h1>
+               <h1>BEWARE, I AM SINNISTAR! THIS IS THE {lesson.title} Lesson!</h1>
                <div>
                    <img src="/images/hatCard.jpg" alt="Unknown Warlock" />
                    <div>
                    Photo by <a href="'https://unsplash.com/@ah1?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'">Amirhossein Hasani</a> on <a href="https://unsplash.com/s/photos/magician?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                    </div>
                </div>
-               <img src={lesson.image} alt={lesson.name} />
+               <img src={lesson.image} alt={lesson.title} />
             
                <p>{lesson.content}</p>
                 
