919c1d1d3f0ccac0e90bf40dcd69aed3bcca3e1d
Wyatt Hart <wyatt.c.hart@gmail.com>
2022-03-30 20:46:01 -0700

    Restructured app to have clear Server and Client folders, changed Lesson Hub to read data from Mongo database

---
+++ server/index.js
@@ -0,0 +1,33 @@
+//Modules and Globals
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+//Express Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+//Controllers & Routes
+app.use('/lesson_view', require('./controllers/lessons'))
+
+
+// GET /
+app.get('/', (req, res) => {
+    res.render('home')
+
+})
+
+app.get('*', (req, res) => {
+    res.render('error404')
+})
+
+app.listen(process.env.PORT, function (){
+    console.log("I LIVE AGAIN!")
+})
---
+++ server/models/index.js
@@ -0,0 +1,32 @@
+// Base setup for Mongo integration
+// creator
+// keyword
+// informational
+// mongodb
+
+
+// Documents and prime/foreign keys:
+
+// Creator- 
+
+// Keywords/Type- mongo, express, etc
+
+// Genre- game, tool/resource, lesson
+
+// Title- sortable by all other tables, "master list" of end pages on the site alphabetically by title
+
+
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}, () => {
+        console.log("Successful database connection made", process.env.MONGO_URI)
+      }
+
+)
+
+
+module.exports.Lessons = require('./lessons')
