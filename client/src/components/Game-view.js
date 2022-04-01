6ba75776166246aafe36043541f150163bca25a2
metaversepeace <89940958+metaversepeace@users.noreply.github.com>
2022-04-01 12:52:17 -0700

    another one

--- client/src/components/Game-view.js
+++ client/src/components/Game-view.js
@@ -5,13 +5,10 @@ const React = require('react')
 export default function Game () {
    return (
        <Def>
-           <main>
+           <main className='lesson-view'>
                <h1>This is the {game.title} Game!</h1>
                <div>
-                   <img src="/images/hatCard.jpg" alt="Unknown Warlock" />
-                   <div>
-                   Photo by <a href="'https://unsplash.com/@ah1?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'">Amirhossein Hasani</a> on <a href="https://unsplash.com/s/photos/magician?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-                   </div>
+                   {/*game here*/}
                </div>
                <img src={game.image} alt={game.title} />
             
@@ -19,7 +16,7 @@ export default function Game () {
                    {game.content}
                </div>
                 
-               <a href={`/lessons/${lesson.id}/show`}><button>Need Another Lesson?</button></a>   
+               <a href={`/lessons/${lesson.id}/show`} className='btn-primary'><button>Need Another Lesson?</button></a>   
                
            </main>
        </Def>
--- client/src/components/Lesson-view.js
+++ client/src/components/Lesson-view.js
@@ -5,13 +5,10 @@ const React = require('react')
 export default function Lesson () {
    return (
        <Def>
-           <main>
+           <main className='lesson-view'>
                <h1>This is the {lesson.title} Lesson!</h1>
                <div>
-                   <img src="/images/hatCard.jpg" alt="Unknown Warlock" />
-                   <div>
-                   Photo by <a href="'https://unsplash.com/@ah1?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText'">Amirhossein Hasani</a> on <a href="https://unsplash.com/s/photos/magician?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-                   </div>
+                   {/*lesson here*/}
                </div>
                <img src={lesson.image} alt={lesson.title} />
             
@@ -19,7 +16,7 @@ export default function Lesson () {
                    {lesson.content}
                </p>
                 
-               <a href={`/games/${game.id}/show`}><button>Try A Drill?</button></a>   
+               <a href={`/games/${game.id}/show`} className='btn-primary'><button>Try A Drill?</button></a>   
                
            </main>
        </Def>
