a8bb5be37907af131650db416a0f6d774cd37fca
Wyatt Hart <wyatt.c.hart@gmail.com>
2022-04-02 01:44:41 -0700

    Links to lesson views now work

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Error404 from './components/error404';
 import GamesHub from './components/Games-Hub';
 import Home from './components/Home';
+import Lesson from './components/Lesson-view';
 import LessonHub from './components/Lessons-Hub';
 
 function App() {
@@ -12,7 +13,10 @@ function App() {
         <Routes>
           <Route path='*' element={<Error404 />}/>
           <Route path='/' element={<Home />} />
-          <Route path='/lessons' element={<LessonHub />} />
+          <Route path='/lessons' element={<LessonHub />}>
+            {/* <Route path=':id' element={<Lesson />} /> */}
+          </Route>
+          <Route path='/lessons/:id' element={<Lesson />} />
           <Route path='/games' element={<GamesHub />} />
         </Routes>
       </header>
--- client/src/components/Lesson-view.js
+++ client/src/components/Lesson-view.js
@@ -1,22 +1,31 @@
 import Def from './default'
 import LessonsHub from './Lessons-Hub'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 const React = require('react')
 
-export default function Lesson () {
+export default function Lesson (props) {
+    let { id } = useParams()
+    const [lesson, setLesson ] = useState({})
+    useEffect(() => {
+        fetch(`/lessons/${id}`)
+            .then((res) => res.json())
+            .then((lesson) => setLesson(lesson))
+    })
    return (
        <Def>
            <main className='lesson-view'>
-               <h1>This is the {lesson.subject} module!</h1>
+               <h1>This is the {lesson.topic} module!</h1>
                <div>
                    {/*lesson here*/}
                </div>
-               <img src={lesson.image} alt={lesson.subject} />
+               <img src={lesson.image} alt={lesson.topic} />
             
                <p>
                    {lesson.content}
                </p>
                 
-               <a href={`/games/${game.id}/show`} className='btn-primary'><button>Try A Drill?</button></a>   
+               {/* <a href={`/games/${game.id}/show`} className='btn-primary'><button>Try A Drill?</button></a>    */}
                
            </main>
        </Def>
