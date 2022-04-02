995080039a0835473ab319c0b156370e5be92870
Wyatt Hart <wyatt.c.hart@gmail.com>
2022-04-02 03:05:46 -0700

    New Lesson view and form are fully working

--- client/src/components/Lessons/new.js
+++ client/src/components/Lessons/new.js
@@ -1,13 +1,22 @@
+import Def from '../default'
+import { useNavigate } from 'react-router-dom'
+
 const React = require('react')
-const Def = require('../default')
 
 export default function NewForm () {
+    let navigate = useNavigate()
+    const handleSubmit = (e) => {
+        navigate(`/lessons`)
+      }
     return (
         <Def>
           <main>
             <h1>Add a New Lesson</h1>
-                <form method="POST" action="/lessons">
-                    
+                <form onSubmit={handleSubmit} method="POST" action="/lessons">
+                    <div className="form-group">
+                        <label htmlFor="name">Creator</label>
+                        <input className="form-control" id="creator" name="creator" required />
+                    </div>
                     <div className="form-group">
                         <label htmlFor="name">Lesson Subject</label>
                         <input className="form-control" id="subject" name="subject" required />
@@ -17,12 +26,36 @@ export default function NewForm () {
                         <label htmlFor="pic">Lesson Title</label>
                         <input className="form-control" id="title" name="title" />
                     </div>
+                    <div className="form-group">
+                        <label htmlFor="pic">Lesson Topic</label>
+                        <input className="form-control" id="topic" name="topic" />
+                    </div>
 
                     <div className="form-group">
                         <label htmlFor="city">Lesson Image</label>
                         <input className="form-control" id="pic" name="pic" />
                     </div>
                     
+                    <div className="form-group">
+                        <label htmlFor="state">Contains Express?</label>
+                        <input className="form-control" id="hasExpress" name="hasExpress" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">Contains Mongo?</label>
+                        <input className="form-control" id="hasMongo" name="hasMongo" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">Contains React?</label>
+                        <input className="form-control" id="hasReact" name="hasReact" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">First Published</label>
+                        <input className="form-control"value={Date()} id="firstPub" name="firstPub" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">Last Published</label>
+                        <input className="form-control" value={Date()} id="lastPub" name="lastPub" />
+                    </div>
                     <div className="form-group">
                         <label htmlFor="state">Lesson Content</label>
                         <input className="form-control" id="content" name="content" />
