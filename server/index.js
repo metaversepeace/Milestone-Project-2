3b1472da65f918ee14d55ca6e31e8e664f8df979
metaversepeace <89940958+metaversepeace@users.noreply.github.com>
2022-04-04 02:15:29 -0700

    fix port

--- server/index.js
+++ server/index.js
@@ -1,40 +1,40 @@
 // Dependencies
-const express = require('express')
-const methodOverride = require('method-override')
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
-const path = require('path')
-const cors = require('cors')
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+const app = express();
+const cors = require('cors');
 
-app.use(cors())
+app.use(cors());
 
 //Express Settings
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
 // app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-app.engine('jsx', require('express-react-views').createEngine())
+app.engine('jsx', require('express-react-views').createEngine());
 
 //Controllers & Routes
-app.use('/lessons', require('./controllers/lessons'))
+app.use('/lessons', require('./controllers/lessons'));
 
 
 // GET /
 app.get('/', (req, res) => {
     res.render('home')
 
-})
+});
 
 app.get('*', (req, res) => {
     res.render('error404')
-})
+});
 
 app.listen(process.env.PORT, function (){
     console.log("I LIVE AGAIN!")
-})
+});
