1e8b814ff7e6a680d8c23d38abc8ba0d3b287eba
metaversepeace <89940958+metaversepeace@users.noreply.github.com>
2022-04-04 01:18:24 -0700

    fix port

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
-//Modules and Globals
-require('dotenv').config()
+// Dependencies
 const express = require('express')
-const path = require('path')
-const cors = require('cors')
 const methodOverride = require('method-override')
 
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
 const app = express()
+const path = require('path')
+const cors = require('cors')
 
 app.use(cors())
 
 //Express Settings
 app.set('views', __dirname + '/views')
-app.set('view engine')
+app.set('view engine', 'jsx')
 // app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_mothod'))
+app.use(methodOverride('_method'))
+
+app.engine('jsx', require('express-react-views').createEngine())
 
 //Controllers & Routes
 app.use('/lessons', require('./controllers/lessons'))
